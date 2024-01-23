5f4707e65631e6352daa65d129e613a6e288b70a
zdungpham <dungphamvan973@gmail.com>
2024-01-23 19:24:32 +0700

    form

---
+++ layout.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Gothic+A1:wght@300&family=Grandstander:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/layout.css"/>
+    <?php include("html/css.html");?>
+    
+</head>
+<body>
+    <?php include("html/header.html");?>
+    <nav class="section nav">
+<div class="container">
+            NAV
+        </div>
+    </nav>
+    <main class="section main">
+        <div class="container">
+            <div class="box-left">
+                <div class="box-left-1">
+                <div class="left-1-top">AAAAAAAAAAAA</div>
+                <div class="left-1-bottom">BBBBBBBBBB</div>
+                </div>
+                <div class="box-left-2">CCCCCCCCCCCCCC</div>
+            </div>
+            <div class="box-right">
+                DDDDDDDDDDDDDD
+            </div>
+        </div>
+    </main>
+    <section class="section">
+        <div class="container">
+            Section
+
+        </div>
+    </section>
+    <footer class="section footer">
+        <div class="container">
+            Footer
+        </div>
+    </footer>
+</body>
+</html>
